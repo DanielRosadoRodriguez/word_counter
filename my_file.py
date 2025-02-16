bcfd9aeb2e53d80950a1dfda3daf295e62b3cceb
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2025-02-16 13:13:48 -0600

    fix: :bug: Removed unused statements and incorrect variable typehint

--- my_file.py
+++ my_file.py
@@ -1,29 +1,30 @@
 from typing import Iterable
-import abc
+from abc import ABC
 
 my_global_var = 3
-a,b = 10
+a, b = 10
 
 
-def sum_even_numbers(numbers: Iterable[int]
-                     ,self
-                     ,aaa) -> int:
-    """Given an iterab
-    return """ """
+def sum_even_numbers(numbers: Iterable[int], self, aaa) -> int:
+    (
+        """Given an iterab
+    return """
+        """
     le of integers, return the sum of all even numbers in the iterable."""
-    my_local_var = 10
-    self.aaaa = 10
-    self.cccc
-    self.b:int = 3
-
-    
-    return sum( 
-        num for num in numbers
-        if (num % 2 == 0)
     )
+    # my_local_var = 10
+    self.a = 10
+    self.c
+    self.b = 3
+
+    return sum(num for num in numbers if (num % 2 == 0))
+
 
 x = 10
-class MyClass:
+
+
+class MyClass(ABC):
     z = 10
+
     def do_thing():
         pass
