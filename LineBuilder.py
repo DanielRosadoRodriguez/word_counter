bee8434734ce7a419e5ff0d602a01d5369d3b571
Arturo Quezada <arturo-andres2011@hotmail.com>
2025-02-16 10:47:53 -0600

    Feat
    Se  agrego el conteo de las lineas fisicas y logicas, se agrego para que pueda saber cuando una linea logica ocupa mas de una fisica. Se agregaron los casos del: RETURN, COMENTARIO, METODOS, VARIABLES

--- LineBuilder.py
+++ LineBuilder.py
@@ -2,9 +2,13 @@ import re
 from Line import Line
 
 class LineBuilder:
+
+    _raw_lines : list[str]
+    _built_lines: list[Line]
+
     def __init__(self, raw_lines: list[str]):
-        self._raw_lines: list[str] = raw_lines
-        self._built_lines: list[Line] = self._build_lines()
+        self._raw_lines = raw_lines
+        self._built_lines= self._build_lines()
 
     def _build_lines(self) -> list[Line]:
         result: list[Line] = []
@@ -12,16 +16,26 @@ class LineBuilder:
         while i < len(self._raw_lines):
             line: str = self._raw_lines[i]
             stripped: str = line.strip()
-            if self._is_method_signature(stripped):
-                result.append(Line("method signature", 1, 1))
+            if self._is_comment(stripped):
+                i = self._skip_comment(i)
+            elif self._is_method_signature(stripped):
+                _physical_Line : int = self._count_physical_lines(":",i) 
+                i = _physical_Line - 1 + i
+                result.append(Line("method signature",_physical_Line, 1))
             elif self._is_import_line(stripped):
                 result.append(Line("import",1, 1))
             elif self._is_class(stripped):
                 result.append(Line("class", 1, 1))
-            elif self._is_global_variable(stripped):
-                result.append(Line("global variable", 1, 1))
+            elif self._is_variable(stripped) or self._is_self_attribute(stripped):
+                result.append(Line("variable", 1, 1))
+            elif self._is_return(stripped):
+                _physical_Line : int = 1 if stripped.count("(") == 0 else self._count_physical_lines(")",i)
+                i = _physical_Line - 1 + i
+                result.append(Line("return", _physical_Line, 0))
             i += 1
         return result
+    
+
     
     def _is_import_line(self, stripped: str) -> bool:
         starts_with_import: bool = stripped.startswith("import")
@@ -34,9 +48,61 @@ class LineBuilder:
     def _is_method_signature(self, stripped: str) -> bool:
         return stripped.startswith("def ")
     
-    def _is_global_variable(self, stripped: str) -> bool:
-        pattern:re.Pattern = r'^[a-zA-Z_]\w*\s*(?::\s*\w+\s*)?=(?!=)'
-        return bool(re.match(pattern, stripped))
+    def _is_return(self, stripped: str) -> bool:
+        return stripped.startswith("return")
 
+    def _is_variable(self, stripped: str) -> bool:
+        pattern:re.Pattern = r'^[a-zA-Z_]\w*(\s*,\s*[a-zA-Z_]\w*)*\s*=\s*(?![=])'
+        return bool(re.match(pattern, stripped))
+        
+    def _is_self_attribute(self, stripped: str) -> bool:
+        if stripped.startswith("self."):
+            if "=" in stripped:
+                if "(" not in stripped.split("=")[0]: 
+                    return True
+            elif "(" not in stripped:
+                return True
+        return False
+
+
+
+    def _is_comment(self, stripped : str):
+        return stripped.startswith('"""')
+
+    def is_indented(self,line: str) -> bool:
+        return bool(re.match(r'^\s+', line))
+    
+    def _get_indentation(self, line: str) -> int:
+        return len(line) - len(line.lstrip())
+
+
+    def _skip_comment(self,i):
+        return self._count_physical_lines('"""',i) - 1 + i
+    
+    def _skip_method(self,startIndex : int):
+        j = startIndex
+        while j < len(self._raw_lines):
+            _line: str = self._raw_lines[j].strip()
+            if (self._is_class(_line) or self._is_method_signature()):
+                break
+            j += 1
+        return j - startIndex + 1
+    
+    def _count_physical_lines(self,endChar : str, startIndex : int = 0):
+        j = startIndex
+        open_par = 0
+        while j < len(self._raw_lines):
+            _line: str = self._raw_lines[j].strip()
+            if(endChar == ")"):
+                open_par += _line.count("(")
+                open_par -= _line.count(")")
+            elif(endChar == '"""'):
+                open_par += _line.count('"""')
+
+            if (open_par % 2 == 0 and _line.endswith(endChar)):
+                break
+            j += 1
+        return j - startIndex + 1
+    
     def get_built_lines(self) -> list[Line]:
         return self._built_lines
--- my_file.py
+++ my_file.py
@@ -2,15 +2,28 @@ from typing import Iterable
 import abc
 
 my_global_var = 3
+a,b = 10
 
-def sum_even_numbers(numbers: Iterable[int]) -> int:
+
+def sum_even_numbers(numbers: Iterable[int]
+                     ,self
+                     ,aaa) -> int:
     """Given an iterab
+    return """ """
     le of integers, return the sum of all even numbers in the iterable."""
-    return sum(
+    my_local_var = 10
+    self.aaaa = 10
+    self.cccc
+    self.b:int = 3
+
+    
+    return sum( 
         num for num in numbers
-        if num % 2 == 0
+        if (num % 2 == 0)
     )
 
+x = 10
 class MyClass:
+    z = 10
     def do_thing():
         pass
